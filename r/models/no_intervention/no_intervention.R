2560862469ec80dd18a6518b391cd795bc16f189
Max Salvatore <maxsalvatore@Maxs-MacBook-Air.local>
2021-11-03 12:40:59 -0400

    add predicted observed to esir output

--- r/models/no_intervention/no_intervention.R
+++ r/models/no_intervention/no_intervention.R
@@ -69,7 +69,7 @@ setwd(data_repo)
     nburnin        = nburnins
   )
   
-  clean_out <- no_int_mod %>% cleanr_esir(N = N, adj = T, adj_len = 2, name = "No intervention")   
+  clean_out <- no_int_mod %>% cleanr_esir(N = N, adj = T, adj_len = 2, name = "No intervention", out_obs = TRUE)   
   write_tsv(clean_out$data, file = paste0("./", casename, "_data.txt"))
   write_tsv(clean_out$out_tib, file = paste0("./", casename, "_out_table.txt"))
   
